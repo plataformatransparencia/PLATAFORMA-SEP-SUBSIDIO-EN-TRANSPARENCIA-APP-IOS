621151a111df61a17751279ebcea1d1bc9263e8f
Armando <jarodriguezvera@gmail.com>
2025-02-21 17:14:34 -0600

    Add CompromisoUniversidadDetalle

--- sep-subsidio-en-transparencia-ios/EnTransparencia/Universidad/Tablero/Compromisos/CompromisoInforme.swift
+++ sep-subsidio-en-transparencia-ios/EnTransparencia/Universidad/Tablero/Compromisos/CompromisoInforme.swift
@@ -17,7 +17,7 @@ struct CompromisoInforme: View {
     var body: some View {
         NavigationLink(
             destination: {
-                TestListView()
+                CompromisoUniversidadShowDetalle(anio:self.anio,id:self.id,subsidio: self.subsidio,tipo:self.id)
             },
             label: {
                 HStack{
@@ -36,12 +36,82 @@ struct CompromisoInforme: View {
         )
     }
 }
-struct CompromisoUniversidadShowDetaile :View {
+struct CompromisoUniversidadShowDetalle :View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State var anio: Int
+    @State var id: String
+    @State var subsidio: String
+    @State var tipo: String
+    @StateObject private var compromisosUniversidadViewModel = CompromisosUniversidadViewModel()
+    
     var body: some View {
-        
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        VStack{
+            NavigationView{
+                ZStack{
+                    Color.white
+                        .edgesIgnoringSafeArea(.all)
+                    ScrollView(showsIndicators: false){
+                        HStack{
+                            VStack{
+                                HStack{
+                                    Button(action: {
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }, label: {
+                                        Image(systemName: "chevron.left")
+                                            .font(.titulo())
+                                            .foregroundColor(Color("gris1"))
+                                    })
+                                    Spacer()
+                                    Text(TITULO_COMPROMISOS_UNI)
+                                        .foregroundColor(Color("rosita"))
+                                        .font(.titulo())
+                                        .bold()
+                                    Spacer()
+                                }
+                            }
+                            Spacer()
+                        }.onAppear{
+                            compromisosUniversidadViewModel.loadComprmisos(anio: self.anio, id: self.id, subsidio: self.subsidio, tipo: self.tipo)
+                        }
+                        .padding([.horizontal, .top])
+                        .background(Color.white)
+               
+                        if compromisosUniversidadViewModel.compromisos != nil {
+                            ForEach(0..<compromisosUniversidadViewModel.compromisos!.count, id: \.self){item in
+                                NavigationLink(
+                                    destination: DetalleCompromiso(compromiso: compromisosUniversidadViewModel.compromisos![item].compromiso.reemplazo(),
+                                        cumplimiento: compromisosUniversidadViewModel.compromisos![item].cumplimiento!,
+                                         fecha: compromisosUniversidadViewModel.compromisos![item].fecha!,
+                                        observacion: compromisosUniversidadViewModel.compromisos![item].observacion!),
+                                    label: {
+                                        Text("\(toRoman(numero: item+1))")
+                                            .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                            .font(.texto1())
+                                            .foregroundColor(.black)
+                                            .padding(.horizontal, 8)
+                                        HStack {
+                                            Text("\(compromisosUniversidadViewModel.compromisos![item].compromiso.reemplazo())")
+                                                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                                .font(.texto1())
+                                                .foregroundColor(.black)
+                                            Spacer()
+                                            Image(systemName: "chevron.right")
+                                                .font(.texto1())
+                                                .foregroundColor(Color("gris1"))
+                                            
+                                        }.padding([.top,.trailing,.bottom])
+                                        .frame(maxWidth: .infinity, maxHeight: 150)
+                                    })
+                            }
+                        }
+                    }.navigationBarHidden(true)
+                }
+            }.navigationViewStyle(StackNavigationViewStyle())
+            .navigationBarHidden(true)
+        }
     }
 }
+
 
 
 
